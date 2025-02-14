2f546d9b15f58bcc2eb5aae8aa9a808db2d4e182
汪蔚 <wangwei@wangweideMac-mini.local>
2025-02-14 17:35:01 +0800

    update

--- taitan-orm-parser/src/table_def.rs
+++ taitan-orm-parser/src/table_def.rs
@@ -1,19 +1,12 @@
 use crate::attr_parser::{AttrParser, NamedAttribute, NamedAttributes};
 use crate::field_def::FieldDef;
-use serde::de::{SeqAccess, Visitor};
-use serde::{Deserialize, Deserializer, Serialize};
-use std::borrow::Cow;
-use std::collections::HashMap;
-use std::marker::PhantomData;
-use syn::{Attribute, Field};
 use crate::{FieldParser, InputParser};
-use crate::info_parser::IndexParser;
+use case::CaseExt;
+use serde::de::SeqAccess;
+use serde::{Deserializer, Serialize};
+use std::borrow::Cow;
+use syn::DeriveInput;
 
-// #[table(user)]
-// #[serde_struct(entity, location)]
-// #[primary(id)]
-// #[unique(name, age)]
-// #[index(name = xxx, fields = (f1, f2, f3))]
 #[derive(Debug, PartialEq, Clone, Default, Serialize)]
 pub struct TableDef<'a> {
     pub table_name: Cow<'a, str>,
@@ -25,24 +18,120 @@ pub struct TableDef<'a> {
 }
 
 impl<'a> TableDef<'a> {
-    pub fn new(fields: &'a [Field], attrs: &'a [Attribute]) -> TableDef<'a> {
+    pub fn new(input: &'a DeriveInput) -> TableDef<'a> {
+        let struct_name = input.ident.to_string();
+        let table_name_attr: Option<NamedAttribute> = AttrParser::extract(&input.attrs, "table");
+        let table_name = if let Some(attr) = &table_name_attr {
+            attr.get_single_value()
+        } else {
+            &Cow::Owned(struct_name.to_snake())
+        };
+        let serde_attr: Option<NamedAttribute> = AttrParser::extract(&input.attrs, "serde_struct");
+        let serde_structs = serde_attr.map(|attr| attr.values).unwrap_or_default();
+
+        let fields = InputParser::get_fields(&input.data);
+        let attrs = &input.attrs;
+
         let fields_def: Vec<FieldDef> = fields.iter().map(|f| FieldParser::parse(f)).collect();
         let primary_attr = AttrParser::extract_one(attrs, "primary");
-
+        let uniques_attrs = AttrParser::extract_multi_list(attrs, "unique");
+        let uniques = uniques_attrs
+            .into_iter()
+            .map(NamedFieldsGroup::from)
+            .collect();
+        let index_attrs = AttrParser::extract_multi_list(attrs, "index");
+        let indexes = index_attrs
+            .into_iter()
+            .map(NamedFieldsGroup::from)
+            .collect();
         Self {
+            table_name: table_name.clone(),
+            serde_structs,
             fields: fields_def,
             primary_fields: primary_attr.values,
-            ..Default::default()
+            uniques,
+            indexes,
         }
+    }
 
+    pub fn get_primary_fields(&'a self) -> Vec<&'a FieldDef<'a>> {
+        self.filter(&self.primary_fields)
+    }
+
+    pub fn get_not_primary_fields(&'a self) -> Vec<&'a FieldDef<'a>> {
+        self.filter_not_in(&self.primary_fields)
+    }
+
+    pub fn get_unique_names(&'a self) -> Vec<Cow<'a, str>> {
+        self.uniques.iter().map(|u| u.name.clone()).collect()
+    }
+
+    pub fn get_index_names(&'a self) -> Vec<Cow<'a, str>> {
+        self.indexes.iter().map(|u| u.name.clone()).collect()
+    }
+
+    pub fn get_unique_fields(&'a self, name: &str) -> Vec<&'a FieldDef<'a>> {
+        let unique = self.uniques.iter().find(|u| u.name == name);
+        if let Some(unique) = unique {
+            self.filter(&unique.fields)
+        } else {
+            Vec::new()
+        }
+    }
+
+    pub fn get_index_fields(&'a self, name: &str) -> Vec<&'a FieldDef<'a>> {
+        let index = self.indexes.iter().find(|u| u.name == name);
+        if let Some(index) = index {
+            self.filter(&index.fields)
+        } else {
+            Vec::new()
+        }
+    }
+
+    fn filter(&'a self, names: &[Cow<'a, str>]) -> Vec<&'a FieldDef<'a>> {
+        self.fields
+            .iter()
+            .filter(|f| names.iter().any(|a| a == f.column_name()))
+            .collect::<Vec<_>>()
+    }
+
+    fn filter_not_in(&'a self, names: &[Cow<'a, str>]) -> Vec<&'a FieldDef<'a>> {
+        self.fields
+            .iter()
+            .filter(|f| names.iter().all(|a| a != f.column_name()))
+            .collect::<Vec<_>>()
     }
 }
-
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize)]
 pub struct NamedFieldsGroup<'a> {
     pub name: Cow<'a, str>,
     pub fields: Vec<Cow<'a, str>>,
+}
+
+impl<'a> From<NamedAttributes<'a>> for NamedFieldsGroup<'a> {
+    fn from(attrs: NamedAttributes<'a>) -> NamedFieldsGroup<'a> {
+        let mut named = NamedFieldsGroup::default();
+        for attr in attrs.attrs.iter() {
+            if attr.name == "name" {
+                named.name = attr.values.get(0).unwrap().clone();
+            }
+            if attr.name == "fields" {
+                let fields = &attr.values;
+                named.fields = fields.clone();
+            }
+        }
+        named
+    }
+}
+
+impl<'a> From<NamedAttribute<'a>> for NamedFieldsGroup<'a> {
+    fn from(attr: NamedAttribute<'a>) -> NamedFieldsGroup<'a> {
+        NamedFieldsGroup {
+            name: attr.name,
+            fields: attr.values,
+        }
+    }
 }
 
 // impl<'a> NamedFieldsGroup<'a> {
