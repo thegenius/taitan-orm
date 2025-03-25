138a1a321546e5f6931a18833c5de4cd1d62568c
汪蔚 <wangwei@wangweideMac-mini.local>
2025-03-25 10:22:26 +0800

    update

--- taitan-orm/src/database/mysql/mod.rs
+++ taitan-orm/src/database/mysql/mod.rs
@@ -3,15 +3,20 @@ mod database;
 mod transaction;
 
 pub mod api;
-pub (crate) mod generator;
+pub(crate) mod generator;
 
 pub use builder::MySqlBuilder;
-pub use transaction::MySqlTransaction;
 pub use database::MySqlDatabase;
+pub use transaction::MySqlTransaction;
 
-// pub use api::ReaderApiNew;
-// pub use api::ReaderMutApiNew;
-// pub use api::WriterApiNew;
-// pub use api::WriterMutApiNew;
-// pub use api::TemplateApiNew;
-// pub use api::TemplateMutApiNew;
+pub mod prelude {
+    pub use crate::database::mysql::api::ReaderApiNew;
+    pub use crate::database::mysql::api::ReaderMutApiNew;
+    pub use crate::database::mysql::api::TemplateApiNew;
+    pub use crate::database::mysql::api::TemplateMutApiNew;
+    pub use crate::database::mysql::api::WriterApiNew;
+    pub use crate::database::mysql::api::WriterMutApiNew;
+    pub use crate::database::mysql::builder::MySqlBuilder;
+    pub use crate::database::mysql::database::MySqlDatabase;
+    pub use crate::database::mysql::transaction::MySqlTransaction;
+}
--- taitan-orm/src/database/postgres/mod.rs
+++ taitan-orm/src/database/postgres/mod.rs
@@ -9,9 +9,14 @@ pub use database::PostgresDatabase;
 pub use transaction::PostgresTransaction;
 pub use builder::PostgresBuilder;
 
-// pub use api::ReaderApiNew;
-// pub use api::ReaderMutApiNew;
-// pub use api::WriterApiNew;
-// pub use api::WriterMutApiNew;
-// pub use api::TemplateApiNew;
-// pub use api::TemplateMutApiNew;
+pub mod prelude {
+    pub use crate::database::postgres::api::ReaderApiNew;
+    pub use crate::database::postgres::api::ReaderMutApiNew;
+    pub use crate::database::postgres::api::TemplateApiNew;
+    pub use crate::database::postgres::api::TemplateMutApiNew;
+    pub use crate::database::postgres::api::WriterApiNew;
+    pub use crate::database::postgres::api::WriterMutApiNew;
+    pub use crate::database::postgres::builder::PostgresBuilder;
+    pub use crate::database::postgres::database::PostgresDatabase;
+    pub use crate::database::postgres::transaction::PostgresTransaction;
+}
